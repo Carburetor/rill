9cc60cb3d1a85940b2745a652f18225787557010
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2018-11-25 02:18:21 -0800

    Add generic write implementation

--- lib/rill/message_store.ex
+++ lib/rill/message_store.ex
@@ -1,4 +1,6 @@
 defmodule Rill.MessageStore do
+  alias Rill.Messaging.Message.Transform
+
   @type read_option(
           {:position, non_neg_integer()}
           | {:batch_size, pos_integer()}
@@ -28,4 +30,35 @@ defmodule Rill.MessageStore do
               message :: struct(),
               stream_name :: String.t()
             ) :: non_neg_integer()
+
+  # @spec write(
+  #         session :: term(),
+  #         database :: module(),
+  #         messages :: struct() | [struct()],
+  #         stream_name :: String.t(),
+  #         opts :: [write_option()]
+  #       ) :: non_neg_integer()
+  # def write(session, database, message, stream_name)
+  #     when not is_list(message) do
+  #   write(session, database, message, stream_name, [])
+  # end
+
+  # def write(session, database, message, stream_name, opts)
+  #     when not is_list(message) do
+  #   write(session, database, [message], stream_name, opts)
+  # end
+
+  # def write(session, database, messages, stream_name) when is_list(messages) do
+  #   write(session, database, messages, stream_name, [])
+  # end
+
+  # def write(session, database, messages, stream_name, opts)
+  #     when is_list(messages) do
+  #   message_data = Transform.write(message)
+  #   expected_version = Keyword.get(opts, :expected_version)
+  #   expected_version = ExpectedVersion.canonize(expected_version)
+  #   # TODO:
+
+  #   database.put(session, message_data, stream_name)
+  # end
 end
